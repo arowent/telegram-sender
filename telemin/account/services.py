a21acfef50e22e090bacafe7f0f11f8a81a70b78
arowent <arowent@mail.ru>
2022-04-23 02:22:14 +0300

    update

--- telemin/account/services.py
+++ telemin/account/services.py
@@ -1,11 +1,20 @@
+from distutils.command.upload import upload
 import os
 from django.contrib import messages
 
-# The maximum file size is 2 Mb, the size is specified in bytes
-MAX_SIZE = 2097152
+# The maximum file size is 5Mb, the size is specified in bytes
+# MAX_SIZE = 5242880
+MAX_SIZE = 2303741
+
 
 
 def user_photo_size_verify(instance, filename):
     """We check the size of the downloaded file and the extension."""
-    pass
-    # messages.error(request, 'Размер файла не должен превышать 2Mb.')
+    file_size = instance.photo.size
+    fname, extension = os.path.splitext(filename)
+
+    if file_size <= MAX_SIZE and extension in ['.png', '.jpg', '.jpeg', '.gif']:
+        upload = '{}/{}'.format(instance.user, filename)
+    return upload
+    # print(f'Instance: {instance.photo.size} | dir: {dir(instance)}')
+    # messages.error(request, 'Размер файла не должен превышать 5Mb.')
