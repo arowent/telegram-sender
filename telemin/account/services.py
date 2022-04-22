420a3545ff8847c0da056640e85226ace5a67a92
arowent <arowent@mail.ru>
2022-04-23 01:45:27 +0300

    update

--- telemin/account/services.py
+++ telemin/account/services.py
@@ -1,8 +1,11 @@
 import os
+from django.contrib import messages
 
 # The maximum file size is 2 Mb, the size is specified in bytes
 MAX_SIZE = 2097152
 
 
 def user_photo_size_verify(instance, filename):
+    """We check the size of the downloaded file and the extension."""
     pass
+    # messages.error(request, 'Размер файла не должен превышать 2Mb.')
