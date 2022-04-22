11fb9794f5760d14407b6bb679f782c7d6fde5be
arowent <arowent@mail.ru>
2022-04-23 01:34:58 +0300

    update

--- telemin/account/models.py
+++ telemin/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from . import services
 
 
 class Telegram_Channels(models.Model):
@@ -8,4 +9,4 @@ class Telegram_Channels(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    photo = models.FileField(upload_to=services.user_photo_size_verify, blank=True)
---
+++ telemin/account/services.py
@@ -0,0 +1,8 @@
+import os
+
+# The maximum file size is 2 Mb, the size is specified in bytes
+MAX_SIZE = 2097152
+
+
+def user_photo_size_verify(instance, filename):
+    pass
