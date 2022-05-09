6cabf4ddba7bf5a1445c46dc545a906af478df09
arowent <arowent@mail.ru>
2022-05-09 19:19:33 +0300

    update

--- telemin/account/urls.py
+++ telemin/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-
+    path('profile/', TemplateView.as_view(template_name='account/profile.html'))
 ]
--- telemin/account/views.py
+++ telemin/account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Profile
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def profile_page(request):
+    user = User
+    context = {
+        'user': user
+    }
+    return render(request, 'account/profile.html', context)
